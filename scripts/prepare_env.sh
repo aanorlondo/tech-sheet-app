1dafdaf3a07b976b95eccd9ad050ce2c7b8aa3da
aanorlondo <khaled.khebbeb@gmail.com>
2023-06-06 20:07:37 +0200

    feat: added authorization check on POST

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -12,3 +12,7 @@ export PSQL_DBNAME=psql_dev
 export PSQL_TABLE=app_details
 export PSQL_USER=app_user
 export PSQL_PSWD=appdetails
+
+# AUTH SERVER
+export AUTH_HOST=$(hostname)
+export AUTH_SERVER="https://${AUTH_HOST}/go-auth"
