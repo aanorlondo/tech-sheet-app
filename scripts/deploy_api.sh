2d604cb96c42a6b7030900908d727f5b23cb322a
aanorlondo <khaled.khebbeb@gmail.com>
2024-10-19 15:52:36 +0200

    fix: container unable to reach host

--- scripts/deploy_api.sh
+++ scripts/deploy_api.sh
@@ -4,6 +4,9 @@ source prepare_env.sh
 docker build -t negan/appdetails_api:rasp4 ../flask-app
 docker push negan/appdetails_api:rasp4
 
+host_ip=$(hostname -I | awk '{print $1}')
+hostname=$(hostname)
+
 docker rm -f APPDETAILS-API-LOCAL
 docker run \
     -d \
@@ -15,5 +18,7 @@ docker run \
     -e PSQL_PSWD=${PSQL_PSWD} \
     -e AUTH_SERVER=${AUTH_SERVER} \
     -p 3500:3500 \
+    --add-host="host:${host_ip}" \
+    --add-host="${hostname}:${host_ip}" \
     --name APPDETAILS-API-LOCAL \
     negan/appdetails_api:rasp4
